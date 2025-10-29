9702df83c27302056f692d2be6e88a8b6be2314c
rerere-bit <mhmmdzaarr@gmail.com>
2025-10-29 23:06:55 +0800

    Progress: Uploading file config.txt into ESP32 and adding listDir funtion into main program to list all files and directories of ESP32.

--- 01-filesystem-management-esp32/src/main.cpp
+++ 01-filesystem-management-esp32/src/main.cpp
@@ -1,6 +1,34 @@
 #include <Arduino.h>
 #include "LITTLEFS.h"
 
+void listDir(const char *dirname, uint8_t levels = 0){
+  Serial.printf("Listing directory %s\n", dirname);
+  File root = LITTLEFS.open(dirname);
+  if (!root){
+    Serial.println("Failed to open directory.");
+    return;
+  }
+  if(!root.isDirectory()){
+    Serial.println("Not a directory.");
+    return;
+  }
+
+  File file = root.openNextFile();
+  while (file){
+    Serial.println("DIR: ");
+    Serial.println(file.name());
+    if(levels){
+      listDir(file.name(), levels-1);
+    }
+    else{
+      Serial.print("File: ");
+      Serial.println(file.name());
+      Serial.print("\t Size: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+}
 void setup()
 {
   Serial.begin(9600);
@@ -11,6 +39,8 @@ void setup()
     return;
   }
 
+  listDir("/");
+
   File file = LITTLEFS.open("/sample.txt", "r", false);
   if (!file)
   {
@@ -18,7 +48,7 @@ void setup()
     return;
   }
 
-  Serial.println("File content: ");
+  Serial.println("File content of /sample.txt: ");
   while (file.available())
   {
     Serial.write(file.read());
