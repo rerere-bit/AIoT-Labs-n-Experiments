4958faa0be34f506c00bbf6716da61f02a3505b2
rerere-bit <mhmmdzaarr@gmail.com>
2025-10-29 22:54:36 +0800

    Progress: Uploading file sample.txt into ESP32 and make a program to read sample.txt.

--- 01-filesystem-management-esp32/src/main.cpp
+++ 01-filesystem-management-esp32/src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include "LITTLEFS.h"
 
-// put function declarations here:
-int myFunction(int, int);
+void setup()
+{
+  Serial.begin(9600);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  if (!LITTLEFS.begin(true))
+  {
+    Serial.println("An error has occurred while mounting LittleFS.");
+    return;
+  }
+
+  File file = LITTLEFS.open("/sample.txt", "r", false);
+  if (!file)
+  {
+    Serial.println("Failed to open file for reading.");
+    return;
+  }
+
+  Serial.println("File content: ");
+  while (file.available())
+  {
+    Serial.write(file.read());
+  }
+  file.close();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
 }
