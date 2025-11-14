06475a0a484083a59bd1cde6ea27420922a6a8d4
rerere-bit <mhmmdzaarr@gmail.com>
2025-11-14 11:34:01 +0800

    Completed: Actuator Motor Servo Experiment

--- actuator_motor_servo_esp32/src/main.cpp
+++ actuator_motor_servo_esp32/src/main.cpp
@@ -1,10 +1,31 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
-void setup(){
+Servo myservo; //Membuat objek servo
 
+int servoPin = 13; //Inisiasi pin data servo
+
+int pos = 0; //Inisiasi posisi servo (derajat)
+
+//Menghubungkan ke servo
+void setup(){
+  Serial.begin(115200);
+  Serial.println("Menyambungkan ke Motor Servo!");
+  myservo.attach(servoPin);
 }
 
 void loop(){
+  //Gerakkan dari 0 ke 180 derajat
+  for(pos = 0; pos <= 180; pos += 1){
 
+    myservo.write(pos);
+    delay(15);
+  }
+
+  //Gerakkan dari 180 ke o derajat
+  for(pos = 180; pos >= 0; pos -= 1){
+
+    myservo.write(pos);
+    delay(15);
+  }
 }
